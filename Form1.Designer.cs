724c85de9440846cbcff1082c911c7fa02e6e6d4
Palash Karmaker <karmaker.palash@jivainfotech.com>
2020-10-31 23:26:14 +0530

    Form added, wordpair added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,112 @@ namespace WordFrequencyCalculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Url";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(445, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-9";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(515, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Count Word Frequency";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(475, 365);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(662, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Count Word Pair Frequency";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(42, 44);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(876, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(515, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(403, 364);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(930, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using WordProcessor;
 
 namespace WordFrequencyCalculator
 {
@@ -15,7 +11,80 @@ namespace WordFrequencyCalculator
         public Form1()
         {
             InitializeComponent();
+            backgroundWorker1.DoWork += BackgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += BackgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerCompleted += BackgroundWorker1_RunWorkerCompleted;
+        }
+        Uri link;
+        private void button1_Click(object sender, EventArgs e) => ButtonClick(1);
+
+        private void ButtonClick(object arg)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+                return;
+            try
+            {
+                link = new Uri(textBox1.Text);
+                ToggleWorking(true);
+                backgroundWorker1.RunWorkerAsync(arg);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid url", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                backgroundWorker1.CancelAsync();
+            }
         }
 
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 100;
+            ToggleWorking(false);
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            if(e.ProgressPercentage == 50)
+                richTextBox1.Text = e.UserState.ToString();
+            if(e.ProgressPercentage == 75)
+                BindList(e.UserState as IEnumerable<PhraseFrequency>);
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var fc = new FrequencyCalculator();
+            var content = Utility.GetTextFromUrl(new Uri(textBox1.Text));
+            backgroundWorker1.ReportProgress(50, content);
+            IEnumerable<PhraseFrequency> res;
+            if(e.Argument.ToString() == "1")
+                res = fc.CalculateWordFrequence(content, 10);
+            else
+                res = fc.CalculateWordPairFrequency(content, 10);
+            backgroundWorker1.ReportProgress(75, res);
+            backgroundWorker1.ReportProgress(100);
+        }
+
+        private void BindList(IEnumerable<PhraseFrequency> res)
+        {
+            foreach (var w in res)
+                listBox1.Items.Add($"{w.Word} ({w.Frequency})\r\n");
+        }
+
+        private void ToggleWorking(bool working)
+        {
+            textBox1.Enabled = !working;
+            button1.Enabled = !working;
+            button2.Enabled = !working;
+            richTextBox1.Enabled = !working;
+            if (working)
+            {
+                progressBar1.Value = 0;
+                listBox1.Items.Clear();
+                richTextBox1.Text = string.Empty;
+            }
+            listBox1.Enabled = !working;
+        }
+
+        private void button2_Click(object sender, EventArgs e) => ButtonClick(2);
     }
 }
